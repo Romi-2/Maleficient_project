3ebb83f2c02e9ef80bf4563057072eec314c4972
Romi <romesa.arshad.baloch@gmail.com>
2026-04-13 12:53:25 +0500

    Remove React - deploy as static HTML/CSS/JS site

---
+++ src/index.js
@@ -0,0 +1,21 @@
+﻿import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+// If you have existing JavaScript files, you can import them here
+// For now, let's create a basic structure
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Maleficient Project</h1>
+      <p>Welcome to my website!</p>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
