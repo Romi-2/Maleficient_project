c9ead5e81a1ff438c3c88c2128a1777f4edbaf0f
Romi <romesa.arshad.baloch@gmail.com>
2026-04-13 13:20:47 +0500

    Complete React setup: all required files added (src, public, configs)

---
+++ src/App.js
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div style={{ 
+      textAlign: 'center', 
+      padding: '50px',
+      fontFamily: 'Arial, sans-serif'
+    }}>
+      <h1>Maleficient Project</h1>
+      <p>React application is running successfully!</p>
+      <p>?? Deployed on Netlify</p>
+    </div>
+  );
+}
+
+export default App;
